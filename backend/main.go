a2f6fde7870270d2a10a674ff56ef09abd85d602
adamkoro <akoronics@protonmail.com>
2022-12-15 23:00:20 +0100

    feat: :fire: api endpoints
    /v1 api with a simple ping endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"nuxt3-golang-jwt-backend/api"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/ping", api.Ping)
+	}
+
+	router.Run("127.0.0.1:8080")
 }
